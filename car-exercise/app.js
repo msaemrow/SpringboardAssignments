c260400be2dc4b4b44cea84230dab0903698986d
Matt Saemrow <mattsaemrow@gmail.com>
2023-11-28 14:04:09 -0600

    finished OOP car exercise

---
+++ car-exercise/app.js
@@ -0,0 +1,50 @@
+class Vehicle{
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk() {
+        return "Beep";
+    }
+
+    toString(){
+        return `This vehicle is a ${this.make} ${this.model} from ${this.year}`
+    }
+}
+
+class Car extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWHeels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWHeels = 2;
+    }
+
+    revEngine(){
+        return "VROOM!!!";
+    }
+}
+
+class Garage{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+
+    add(newVehicle){
+        if(this.vehicles.length >= this.capacity){
+            return "Sorry, we're full."
+        } else if(typeof newVehicle !== "object"){
+            return "Sorry, only vehicles are allowed in here!"
+        } 
+        this.vehicles.push(obj);
+        return "Vehicle added!"
+    }
+}
