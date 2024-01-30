09a23546222325e57beef415ec701ef39148d1f7
Matt Saemrow <mattsaemrow@gmail.com>
2024-01-30 12:32:40 -0600

    finished part 2 of sql ddl exercise

--- sql-ddl-design/outer_space.sql
+++ sql-ddl-design/outer_space.sql
@@ -7,22 +7,96 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
-CREATE TABLE planets
+CREATE TABLE galaxy
 (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy_name TEXT NOT NULL
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+CREATE TABLE planet
+(
+  id SERIAL PRIMARY KEY,
+  planet_name TEXT NOT NULL,
+  galaxy_id INTEGER REFERENCES galaxy
+);
+
+CREATE TABLE orbit
+(
+  id SERIAL PRIMARY KEY,
+  planet_id INTEGER REFERENCES planet,
+  orbital_period_in_years FLOAT NOT NULL,
+  orbits_around TEXT NOT NULL
+);
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  moon_name TEXT NOT NULL,
+  planet_id INTEGER REFERENCES planet
+);
+
+
+INSERT INTO galaxy
+(galaxy_name)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+('Milky Way');
+
+INSERT INTO planet
+(planet_name, galaxy_id)
+VALUES
+('Earth', 1),
+('Mars', 1),
+('Venus', 1),
+('Neptune', 1);
+
+INSERT INTO orbit
+(planet_id, orbital_period_in_years, orbits_around)
+VALUES
+(1, 1.00, 'The Sun'),
+(2, 1.88, 'The Sun'),
+(3, 0.62, 'The Sun'),
+(4, 164.8, 'The Sun');
+
+INSERT INTO moons
+(moon_name, planet_id)
+VALUES
+('The Moon', 1),
+('Phobos', 2),
+('Deimos', 2),
+('Naiad', 4),
+('Thalassa', 4),
+('Despina', 4),
+('Galatea', 4),
+('Larissa', 4),
+('S/2004 N 1', 4),
+('Proteus', 4),
+('Triton', 4), 
+('Nereid', 4),
+('Halimede', 4),
+('Sao', 4),
+('Laomedeia', 4),
+('Psamathe', 4),
+('Neso', 4);
+
+
+--Original table layout
+-- CREATE TABLE planets
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   orbital_period_in_years FLOAT NOT NULL,
+--   orbits_around TEXT NOT NULL,
+--   galaxy TEXT NOT NULL,
+--   moons TEXT[]
+-- );
+
+--ORIGINAL PLANETS TABLE DATA
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
