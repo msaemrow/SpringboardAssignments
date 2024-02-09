d399a4be3594f77dde45686106e80a2644d7db54
Matt Saemrow <mattsaemrow@gmail.com>
2024-02-09 15:03:12 -0600

    finished pet adoption application

--- flask-sql-adopt/app.py
+++ flask-sql-adopt/app.py
@@ -1,6 +1,6 @@
 """Adopt a pet application"""
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy import text
 from models import Pet, db, connect_db
@@ -30,4 +30,26 @@ def process_add_pet():
     new_pet = Pet(name=request.form['name'], species=request.form['species'], photo_url=request.form['photo_url'], age=request.form['age'], notes=request.form['notes'])
     db.session.add(new_pet)
     db.session.commit()
+    flash(f"Successfully added {new_pet.name}. Click on their photo below for more details", 'success')
     return redirect('/')
+
+@app.route('/pet/<int:pet_id>')
+def display_pet_info(pet_id):
+    pet = Pet.query.get(pet_id)
+    return render_template('pet-display.html', pet = pet)
+
+@app.route('/pet/<int:pet_id>/edit', methods=["POST"])
+def process_edit_pet_info(pet_id):
+    pet = Pet.query.get(pet_id)
+    pet.name = request.form['name']
+    pet.species = request.form['species']
+    pet.photo_url = request.form['photo_url']
+    pet.age = request.form['age']
+    pet.notes = request.form['notes']
+    pet.available = True if request.form['available'] == 'True' else False
+
+    db.session.add(pet)
+    db.session.commit()
+    flash(f"Successfully edited {pet.name}'s information", 'success')
+
+    return redirect(f"/pet/{pet_id}")
