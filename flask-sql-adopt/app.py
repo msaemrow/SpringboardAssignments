17b1ff306ac6912a944bde94be415c5f89f55dce
Matt Saemrow <mattsaemrow@gmail.com>
2024-02-20 15:44:48 -0600

    updated form from html to WTForms

--- flask-sql-adopt/app.py
+++ flask-sql-adopt/app.py
@@ -18,44 +18,45 @@ debug_toolbar = DebugToolbarExtension(app)
 connect_db(app)
 
 @app.route('/')
-def home_page():
+def display_home_page():
+    """renders home page showing all pets divided into 2 sections- available and pending adoption"""
     pets = Pet.query.all()
     return render_template('pet-all-home.html', pets=pets)
 
 @app.route('/add', methods=["GET", "POST"])
-def process_add_pet():
+def show_and_process_add_pet_form():
+    """Will either show empty add pet form or process submission of add pet form"""
     form = PetInfoForm()
     if form.validate_on_submit():
-        name = form.name.data
-        species = form.price.data
-        print(f"Added {name} and they are a ${species}")
-        return redirect('/')
+        photo_url = "https://i.pinimg.com/564x/36/51/6d/36516d2965ed716b6061e303a3f5a35c.jpg" if form.photo_url.data == "" else form.photo_url.data
+        new_pet = Pet(name=form.name.data, species=form.species.data, photo_url=photo_url, age=form.age.data, notes=form.notes.data)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"Successfully added {new_pet.name}. Click on their photo below for more details", 'success')
+        return redirect('/')  
     else:    
         return render_template('pet-new.html', form=form)
 
-    # new_pet = Pet(name=request.form['name'], species=request.form['species'], photo_url=request.form['photo_url'], age=request.form['age'], notes=request.form['notes'])
-    # db.session.add(new_pet)
-    # db.session.commit()
-    # flash(f"Successfully added {new_pet.name}. Click on their photo below for more details", 'success')
-    # return redirect('/')
 
-@app.route('/pet/<int:pet_id>')
+@app.route('/pet/<int:pet_id>', methods=["GET", "POST"])
 def display_pet_info(pet_id):
+    """displays individual pet info as well as a form to edit the pet information"""
     pet = Pet.query.get(pet_id)
-    return render_template('pet-display.html', pet = pet)
+    form = PetInfoForm(obj=pet)
+    if form.validate_on_submit():
+        pet.name = form.name.data
+        pet.species = form.species.data
+        pet.photo_url = form.photo_url.data
+        pet.age = form.age.data
+        pet.notes = form.notes.data
+        print(form.available.data)
+        pet.available = form.available.data
+        print(pet.available)
 
-@app.route('/pet/<int:pet_id>/edit', methods=["POST"])
-def process_edit_pet_info(pet_id):
-    pet = Pet.query.get(pet_id)
-    pet.name = request.form['name']
-    pet.species = request.form['species']
-    pet.photo_url = request.form['photo_url']
-    pet.age = request.form['age']
-    pet.notes = request.form['notes']
-    pet.available = True if request.form['available'] == 'True' else False
-
-    db.session.add(pet)
-    db.session.commit()
-    flash(f"Successfully edited {pet.name}'s information", 'success')
-
-    return redirect(f"/pet/{pet_id}")
+        db.session.add(pet)
+        db.session.commit()
+        flash(f"Successfully edited {pet.name}'s information", 'success')
+        return redirect(f"/pet/{pet_id}")
+    else:
+        print(pet.available)
+        return render_template('pet-display.html', pet=pet, form=form)
--- flask-sql-adopt/form.py
+++ flask-sql-adopt/form.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, TextAreaField
-from wtforms.validators import InputRequired, Optional
+from wtforms import StringField, BooleanField, IntegerField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange    
 
 
 class PetInfoForm(FlaskForm):
     """Adds or edits the information for the Pet model"""
-    name = StringField("Pet Name")
-    species = StringField("Pet Species")
-    photo_url = StringField("Profile Picture Url")
-    age = IntegerField("Pet age in years")
+    choices = [('Rabbit', 'Rabbit'), ('Duck', 'Duck'), ('Dog', 'Dog'), ('Parrot', 'Parrot')]
+
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = SelectField("Pet Species", choices=choices, validators=[InputRequired()])
+    photo_url = StringField("Profile Picture Url", validators=[URL(message="Please add a valid URL"), Optional()])
+    age = IntegerField("Pet age in years", validators=[InputRequired(), NumberRange(min=0, max=30)])
     available = BooleanField("Available for adoption")
-    notes = TextAreaField("Notes about the pet")
+    notes = TextAreaField("Notes about the pet", validators=[Optional()])
