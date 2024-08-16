0e2fa91dd68f68c0ccaba400d7919e15206c9980
Matt Saemrow <mattsaemrow@gmail.com>
2024-08-16 18:56:44 -0500

    added default value for garage and confirmed meets exercises requirements

--- car-exercise/app.js
+++ car-exercise/app.js
@@ -1,51 +1,51 @@
-class Vehicle{
-    constructor(make, model, year){
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
 
-    honk() {
-        return "Beep";
-    }
+  honk() {
+    return "Beep";
+  }
 
-    toString(){
-        return `This vehicle is a ${this.make} ${this.model} from ${this.year}`
-    }
+  toString() {
+    return `This vehicle is a ${this.make} ${this.model} from ${this.year}`;
+  }
 }
 
-class Car extends Vehicle{
-    constructor(make, model, year){
-        super(make, model, year);
-        this.numWHeels = 4;
-    }
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWHeels = 4;
+  }
 }
 
-class Motorcycle extends Vehicle{
-    constructor(make, model, year){
-        super(make, model, year);
-        this.numWHeels = 2;
-    }
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWHeels = 2;
+  }
 
-    revEngine(){
-        return "VROOM!!!";
-    }
+  revEngine() {
+    return "VROOM!!!";
+  }
 }
 
-class Garage{
-    constructor(capacity){
-        this.capacity = capacity;
-        this.vehicles = [];
-    }
+class Garage {
+  constructor(capacity = 3) {
+    this.capacity = capacity;
+    this.vehicles = [];
+  }
 
-    add(newVehicle){
-        if(this.capacity === 0){
-            return "Sorry, we're full."
-        } else if(typeof newVehicle !== "object"){
-            return "Sorry, only vehicles are allowed in here!"
-        } 
-        this.vehicles.push(newVehicle);
-        this.capacity --;
-        return "Vehicle added!"
+  add(newVehicle) {
+    if (this.capacity === 0) {
+      return "Sorry, we're full.";
+    } else if (typeof newVehicle !== "object") {
+      return "Sorry, only vehicles are allowed in here!";
     }
+    this.vehicles.push(newVehicle);
+    this.capacity--;
+    return "Vehicle added!";
+  }
 }
